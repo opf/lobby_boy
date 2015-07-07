9daab029b48ed32ed97d3a2bc0274731d4d6f357
Markus Kahl <machisuji@googlemail.com>
2015-07-07 11:09:53 +0200

    removed obsolete dependencies

--- lib/lobby_boy/engine.rb
+++ lib/lobby_boy/engine.rb
@@ -2,30 +2,10 @@ module LobbyBoy
   class Engine < ::Rails::Engine
     isolate_namespace LobbyBoy
 
-    config.assets.precompile += %w( jquery.js js.cookie-1.5.1.min.js )
+    config.assets.precompile += %w( js.cookie-1.5.1.min.js )
 
     config.to_prepare do
       require 'lobby_boy/patches/session_management'
-    end
-
-    ##
-    # Use CORS to allow AJAX re-reauthentication.
-    initializer 'lobby_boy.add_middleware' do |app|
-      require 'rack/cors'
-
-      app.middleware.insert_before 0, 'Rack::Cors' do
-        allow do
-          omniauth = ::OmniAuth.config.path_prefix
-
-          origins '*'
-          resource "#{omniauth}/*", headers: :any, methods: [:get], credentials: true
-        end
-
-        allow do
-          origins '*'
-          resource '/session/*', headers: :any, methods: [:get], credentials: true
-        end
-      end
     end
   end
 end
--- lobby_boy.gemspec
+++ lobby_boy.gemspec
@@ -9,16 +9,13 @@ Gem::Specification.new do |s|
   s.email       = ["info@finn.de"]
   s.homepage    = "https://github.com/finnlabs/lobby_boy"
   s.summary     = "Rails engine for OpenIDConnect Session Management"
+  s.licenses    = ['GPLv3']
 
   s.files = Dir["{app,config,db,lib}/**/*"] + ["LICENSE", "Rakefile", "README.md"]
   s.test_files = Dir["test/**/*"]
 
   s.add_dependency 'rails', '>= 3.2.21'
-  s.add_dependency 'jquery-rails'
-  s.add_dependency 'rack-cors', '~> 0.4.0'
 
   s.add_dependency 'omniauth', '~> 1.1'
   s.add_dependency 'omniauth-openid-connect', '>= 0.2'
-
-  s.add_development_dependency 'sqlite3'
 end
